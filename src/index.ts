e0f16aae0493869ce64ef963c623211d4ab2beeb
Constantine Karos <karosc@cdmsmith.com>
2021-01-06 15:57:14 -0500

    Init

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+/**
+ * @packageDocumentation
+ * @module JLTomorrow
+ */
+
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+import { ITranslator } from '@jupyterlab/translation';
+
+/**
+ * A plugin for the Jupyter Light Theme.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'JLTomorrow:plugin',
+  requires: [IThemeManager, ITranslator],
+  activate: (
+    app: JupyterFrontEnd,
+    manager: IThemeManager,
+    translator: ITranslator
+  ) => {
+    const trans = translator.load('jupyterlab');
+    const style = 'JLTomorrow/index.css';
+    manager.register({
+      name: 'JLTomorrow',
+      displayName: trans.__('JLTomorrow'),
+      isLight: true,
+      themeScrollbars: false,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  },
+  autoStart: true
+};
+
+export default plugin;
